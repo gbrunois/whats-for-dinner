8882242eee26721776d8c9c2f5efbc4ffe5d6ee4
GBrunois <brunois.g@sfeir.com>
2019-01-18 14:54:00 +0100

    :wrench: add vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  pwa: {
+    name: "What's for dinner",
+    themeColor: '#06a69a',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'GenerateSW',
+  },
+}
