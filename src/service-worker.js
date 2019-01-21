4aef30c264f32244b85362039b50e6712a847d6a
GBrunois <brunois.g@sfeir.com>
2019-01-21 12:57:17 +0100

    :wrench: use InjectManifest plugin

---
+++ src/service-worker.js
@@ -0,0 +1,9 @@
+workbox.core.setCacheNameDetails({ prefix: 'whats-for-dinner' })
+
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.suppressWarnings()
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
+workbox.routing.registerNavigationRoute('/index.html', {
+  whitelist: [new RegExp('/week')],
+})
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   pwa: {
-    name: "What's for dinner",
+    name: 'whats-for-dinner',
     themeColor: '#06a69a',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
@@ -9,14 +9,7 @@ module.exports = {
     // configure the workbox plugin
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
-      //skipWaiting: true
-      // swSrc is required in InjectManifest mode.
-      //globDirectory: '.',
-      //globPatterns: ['dist/*.{js,png,html,css}'],
-      swSrc: 'public/service-worker.js',
-      // ...other Workbox options...
-      //swSrc: './src/sw.js',
-      //swDest: 'service-worker.js',
+      swSrc: 'src/service-worker.js',
     },
   },
 }
