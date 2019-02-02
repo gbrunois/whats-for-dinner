6e2229c6f5371bebd3c5dbec9d37d25af74bbb7e
GBrunois <brunois.g@sfeir.com>
2019-02-03 00:19:10 +0100

    :tada: init dialogflow function

--- functions/index.js
+++ functions/index.js
@@ -1,41 +1,66 @@
+/* eslint-disable */
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
 'use strict'
 
-// Import the Dialogflow module from the Actions on Google client library.
-const { dialogflow, SignIn } = require('actions-on-google')
-
-// Import the firebase-functions package for deployment.
 const functions = require('firebase-functions')
+const { WebhookClient } = require('dialogflow-fulfillment')
+const { Card, Suggestion } = require('dialogflow-fulfillment')
 
-// Instantiate the Dialogflow client.
-const app = dialogflow({ debug: true })
+process.env.DEBUG = 'dialogflow:debug' // enables lib debugging statements
 
-app.intent('Default Welcome Intent', conv => {
-  console.log('welcomeIntent')
-  conv.ask(new SignIn('To get your account details'))
-})
-
-app.intent('meal intent', (conv, parameters) => {
-  console.log('meal intent: ' + parameters['meal-period'] + parameters['date'])
-  conv.add(`Welcome to my agent!`)
-})
-
-// Create a Dialogflow intent with the `actions_intent_SIGN_IN` event
-app.intent('Get Sign In', (conv, params, signin) => {
-  console.log('signIn')
-  if (signin.status === 'OK') {
-    console.log('userId', conv.user.raw.userId)
-    conv.ask(
-      `I got your account details. your userId is ${
-        conv.user.raw.userId
-      }. What do you want to do next?`
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
+  (request, response) => {
+    const agent = new WebhookClient({ request, response })
+    console.log(
+      'Dialogflow Request headers: ' + JSON.stringify(request.headers)
     )
-  } else {
-    console.log('not signed in')
-    conv.ask(
-      `I won't be able to save your data, but what do you want to do next?`
-    )
+    console.log('Dialogflow Request body: ' + JSON.stringify(request.body))
+
+    function mealIntent(agent) {
+      agent.add(`Welcome to my agent!`)
+    }
+
+    function fallback(agent) {
+      agent.add(`I didn't understand`)
+      agent.add(`I'm sorry, can you try again?`)
+    }
+
+    // // Uncomment and edit to make your own intent handler
+    // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
+    // // below to get this function to be run when a Dialogflow intent is matched
+    // function yourFunctionHandler(agent) {
+    //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
+    //   agent.add(new Card({
+    //       title: `Title: this is a card title`,
+    //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+    //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+    //       buttonText: 'This is a button',
+    //       buttonUrl: 'https://assistant.google.com/'
+    //     })
+    //   );
+    //   agent.add(new Suggestion(`Quick Reply`));
+    //   agent.add(new Suggestion(`Suggestion`));
+    //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+    // }
+
+    // // Uncomment and edit to make your own Google Assistant intent handler
+    // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
+    // // below to get this function to be run when a Dialogflow intent is matched
+    // function googleAssistantHandler(agent) {
+    //   let conv = agent.conv(); // Get Actions on Google library conv instance
+    //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
+    //   agent.add(conv); // Add Actions on Google library responses to your agent's response
+    // }
+    // // See https://github.com/dialogflow/dialogflow-fulfillment-nodejs/tree/master/samples/actions-on-google
+    // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
+
+    // Run the proper function handler based on the matched Dialogflow intent name
+    let intentMap = new Map()
+    intentMap.set('meal intent', mealIntent)
+    // intentMap.set('Default Fallback Intent', fallback);
+    // intentMap.set('your intent name here', yourFunctionHandler);
+    // intentMap.set('your intent name here', googleAssistantHandler);
+    agent.handleRequest(intentMap)
   }
-})
-
-// Set the DialogflowApp object to handle the HTTPS POST request.
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app)
+)
