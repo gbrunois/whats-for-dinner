e496db6ef128a5bccd9c4da3a8abaf2847ea14b9
GBrunois <brunois.g@sfeir.com>
2019-02-14 09:23:06 +0100

    :hammer: refactor cloud function

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,15 +1,11 @@
-'use strict'
-const admin = require('firebase-admin')
-const functions = require('firebase-functions')
-admin.initializeApp()
-
-const auth = admin.auth()
-const db = admin.firestore()
-
-const settings = { /* your settings... */ timestampsInSnapshots: true }
-db.settings(settings)
-
+import * as functions from 'firebase-functions'
 const { dialogflow, SignIn } = require('actions-on-google')
+
+process.env.DEBUG = 'dialogflow:debug' // enables lib debugging statements
+
+import { Api } from './api'
+const api = new Api()
+api.init()
 
 // Instantiate the Dialogflow client.
 const app = dialogflow({
@@ -32,16 +28,14 @@ app.intent('Get Sign In', async (conv, params, signin) => {
   const { email } = conv.user
   if (!conv.data.uid && email) {
     try {
-      conv.data.uid = (await auth.getUserByEmail(email)).uid
+      conv.data.uid = (await api.getUserByEmail(email)).uid
     } catch (e) {
       if (e.code !== 'auth/user-not-found') {
         throw e
       }
       // If the user is not found, create a new Firebase auth user
       // using the email obtained from the Google Assistant
-      conv.data.uid = (await auth.createUser({
-        email,
-      })).uid
+      conv.data.uid = (await api.createUser(email)).uid
     }
   }
   // if (conv.data.uid) {
@@ -53,48 +47,13 @@ app.intent('Get Sign In', async (conv, params, signin) => {
 app.intent('Ask For Meal', async (conv, parameters) => {
   const aDate = '2019-02-10'
   // conv.user.ref contains the id of the record for the user in a Firestore DB
-  const planningRef = await getPrimaryPlanningRef(conv.data.uid)
-  const day = await planningRef
-    .collection('days')
-    .where('date', '==', aDate)
-    .get()
-    .then(querySnapshot => {
-      const result = []
-      querySnapshot.forEach(doc => {
-        const { date, dinner, lunch } = doc.data()
-        const id = doc.id
-        result.push({
-          id,
-          date,
-          dinner,
-          lunch,
-        })
-      })
-      return result
-    })
+  const planningRef = await api.getPrimaryPlanningRef(conv.data.uid)
+  const day = await api.getDay(planningRef, aDate)
 
   console.log('meal intent: ' + parameters['meal-period'] + parameters['date'])
-  conv.close(`On mange du ${day[0].lunch}`)
+  conv.close(`On mange du ${day.lunch}`)
 })
-
-function getPrimaryPlanningRef(userId) {
-  return db
-    .collection('users')
-    .doc(userId)
-    .get()
-    .then(document => {
-      if (document.exists) {
-        return document.data()
-      } else {
-        return this.createUserWithPrimaryPanning(userId)
-          .then(newUserDocumentRef => newUserDocumentRef.get())
-          .then(newUserDocument => newUserDocument.data())
-      }
-    })
-    .then(data => {
-      return (data && data.primaryPlanning) || undefined
-    })
-}
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app)
+
