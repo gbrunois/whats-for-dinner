2557e6c5144c3a2a17f00a70e2329baaf18ad91f
GBrunois <brunois.g@sfeir.com>
2019-06-11 08:52:28 +0200

    :sparkles: add dialogflow conversation

--- packages/functions/src/api.ts
+++ packages/functions/src/api.ts
@@ -1,80 +1,77 @@
-import * as admin from "firebase-admin";
-import { UserRecord } from "firebase-functions/lib/providers/auth";
+import * as admin from 'firebase-admin'
+import { UserRecord } from 'firebase-functions/lib/providers/auth'
 
 export interface IDay {
-  date: string;
-  dinner: string;
-  lunch: string;
-  id: string;
-  createTime: Date;
+  date: string
+  dinner: string
+  lunch: string
+  id: string
+  createTime: Date
 }
 
 export class Api {
-  auth: admin.auth.Auth;
-  db: FirebaseFirestore.Firestore;
+  auth: admin.auth.Auth
+  db: FirebaseFirestore.Firestore
 
   init(): void {
-    admin.initializeApp();
-    this.auth = admin.auth();
-    this.db = admin.firestore();
+    admin.initializeApp()
+    this.auth = admin.auth()
+    this.db = admin.firestore()
 
-    const settings = { timestampsInSnapshots: true };
-    this.db.settings(settings);
+    const settings = { timestampsInSnapshots: true }
+    this.db.settings(settings)
   }
 
   public async getPrimaryPlanningRef(userId: string) {
     return this.db
-      .collection("users")
+      .collection('users')
       .doc(userId)
       .get()
-      .then(document => {
+      .then((document) => {
         if (document.exists) {
-          return document.data();
+          return document.data()
         } else {
           return this.createUserWithPrimaryPanning(userId)
-            .then(newUserDocumentRef => newUserDocumentRef.get())
-            .then(newUserDocument => newUserDocument.data());
+            .then((newUserDocumentRef) => newUserDocumentRef.get())
+            .then((newUserDocument) => newUserDocument.data())
         }
       })
-      .then(data => {
-        return (data && data.primaryPlanning) || undefined;
-      });
+      .then((data) => {
+        return (data && data.primaryPlanning) || undefined
+      })
   }
 
-  public async getDay(
-    planningRef: any,
-    aDate: string
-  ): Promise<IDay | undefined> {
+  public async getDay(planningRef: any, aDate: string): Promise<IDay | undefined> {
     return await planningRef
-      .collection("days")
-      .where("date", "==", aDate)
+      .collection('days')
+      .where('date', '==', aDate)
       .get()
-      .then(querySnapshot => {
-        const result = [];
-        querySnapshot.forEach(doc => {
-          const { date, dinner, lunch } = doc.data();
-          const id = doc.id;
+      .then((querySnapshot) => {
+        const result = []
+        querySnapshot.forEach((doc) => {
+          const { date, dinner, lunch } = doc.data()
+          const id = doc.id
           result.push({
             id,
             date,
             dinner,
-            lunch
-          });
-        });
-        return result;
+            lunch,
+          })
+        })
+        return result
       })
-      .then(days => days[0]);
+      .then((days) => days[0])
   }
 
   public createUserWithPrimaryPanning(userId: any): any {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.')
   }
 
   public createUser(email: string): Promise<UserRecord> {
-    return this.auth.createUser({ email });
+    return this.auth.createUser({ email })
   }
 
   public getUserByEmail(email: string): Promise<UserRecord> {
-    return this.auth.getUserByEmail(email);
+    return this.auth.getUserByEmail(email)
   }
 }
--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,66 +1,122 @@
 import * as functions from 'firebase-functions'
-const { dialogflow, SignIn } = require('actions-on-google')
+
+import { SignIn, dialogflow } from 'actions-on-google'
+
+import { Api } from './api'
+
+const Parameters = {
+  MEAL_PERIOD: 'meal-period',
+  DATE: 'date',
+}
+const responses = {
+  signInError: 'Vous devez vous authentifier pour continuer.',
+  greetUser: (username: string) => `Bienvenue sur What's for dinner ${username}, que veux-tu savoir ?`,
+  sayTodayMeal: (mealPeriod: string, meal: string) => `Ce ${mealPeriod}, vous mangez ${meal}`,
+  sayTodayMeals: (meals: string[]) => `Vous avez prévu ${meals[0]} pour ce midi et ${meals[1]} pour ce soir`,
+  sayTomorrowMeal: (mealPeriod: string, meal: string) => `Demain ${mealPeriod}, vous avez prévu ${meal}`,
+  sayTomorrowMeals: (meals: string[]) =>
+    `Vousa avez prévu ${meals[0]} pour demain midi et ${meals[1]} pour demain soir`,
+  sorryNothingPlanned: `Désolé, rien n'a été planifié`,
+  unhandledError: "Une erreur s'est produite",
+}
 
 process.env.DEBUG = 'dialogflow:debug' // enables lib debugging statements
 
-import { Api } from './api'
 const api = new Api()
 api.init()
 
 // Instantiate the Dialogflow client.
 const app = dialogflow({
   debug: true,
-  clientId:
-    // @todo : do not expose to git but .env
-    '525526066238-rbksspu3fean1uuihn8n95mh64e45tt4.apps.googleusercontent.com',
+  clientId: process.env.DIALOG_FLOW_CLIENT_ID,
 })
 
-app.intent('Default Welcome Intent', conv => {
+app.intent('Default Welcome Intent', (conv) => {
   console.log('welcomeIntent')
   conv.ask(new SignIn())
 })
 
 // Intent that starts the account linking flow.
-app.intent('Get Sign In', async (conv, params, signin) => {
-  if (signin.status !== 'OK') {
-    return conv.close('Vous devez vous authentifier pour quitter.')
+app.intent('Get Sign In', async (conv, params, signIn: any) => {
+  if (signIn.status !== 'OK') {
+    return conv.close(responses.signInError)
   }
   const { email } = conv.user
-  if (!conv.data.uid && email) {
+  const dataUid: string = conv.data['uid']
+  if (!dataUid && email) {
     try {
-      conv.data.uid = (await api.getUserByEmail(email)).uid
-    } catch (e) {
-      if (e.code !== 'auth/user-not-found') {
-        throw e
+      conv.data['uid'] = (await api.getUserByEmail(email)).uid
+    } catch (err) {
+      if (err.code !== 'auth/user-not-found') {
+        throw new Error(`Auth error: ${err}`)
       }
       // If the user is not found, create a new Firebase auth user
       // using the email obtained from the Google Assistant
-      conv.data.uid = (await api.createUser(email)).uid
+      conv.data['uid'] = (await api.createUser(email)).uid
     }
   }
-  return conv.ask(`Bonjour ${conv.user.profile.payload.given_name}`)
+  return conv.ask(responses.greetUser(conv.user.profile.payload.given_name))
 })
 
-app.intent('Ask For Meal', async (conv, parameters: any[]) => {
-  console.log('meal intent: ' + parameters['meal-period'] + parameters['date'])
-  
-  const mealPeriod:string = parameters['meal-period']
-  const date:Date = parameters['date']
+app.intent('Ask For Meal', async (conv, parameters) => {
+  const mealPeriod: string = parameters[Parameters.MEAL_PERIOD] as string //soir
+  const date: string = parameters[Parameters.DATE] as string //aujourd'hui ou ISO date
+  console.log(`Ask for meal ${mealPeriod} ${date}`)
 
-  const planningRef = await api.getPrimaryPlanningRef(conv.data.uid)
-  const day = await api.getDay(planningRef, date.toISOString().substring(0,10))
+  let requestedDate = ''
+  const isToday = date === "aujourd'hui"
+  let isTomorrow = false
+  if (isToday) {
+    requestedDate = new Date().toISOString()
+  } else {
+    // todo set isTomorrow
+    isTomorrow = true
+    requestedDate = date
+  }
+  const dataUid: string = conv.data['uid']
+  const planningRef = await api.getPrimaryPlanningRef(dataUid) //todo traiter erreur
+  const day = await api.getDay(planningRef, requestedDate.substring(0, 10))
 
-  if(day === undefined) {
-    conv.close(`Désolé, rien n'a été planifié`)
+  if (day === undefined) {
+    conv.close(responses.sorryNothingPlanned) // todo : add requested period
   }
-  if(mealPeriod === 'soir') {
-    conv.close(`${day.dinner}`)
+
+  if (mealPeriod === '') {
+    if (isToday) {
+      conv.close(responses.sayTodayMeals([day.lunch, day.dinner]))
+    } else if (isTomorrow) {
+      conv.close(responses.sayTomorrowMeals([day.lunch, day.dinner]))
+    } else {
+      // todo other days
+    }
+  } else {
+    if (mealPeriod === 'soir') {
+      if (isToday) {
+        conv.close(responses.sayTodayMeal(mealPeriod, `${day.dinner}`))
+      } else if (isTomorrow) {
+        conv.close(responses.sayTomorrowMeal(mealPeriod, `${day.dinner}`))
+      } else {
+        // todo other days
+      }
+    } else if (mealPeriod === 'midi') {
+      if (isToday) {
+        conv.close(responses.sayTodayMeal(mealPeriod, `${day.lunch}`))
+      } else if (isTomorrow) {
+        conv.close(responses.sayTomorrowMeal(mealPeriod, `${day.lunch}`))
+      } else {
+        // todo other days
+      }
+    } else {
+      //todo throw unknown period
+    }
   }
-  if(mealPeriod === 'midi') {
-    conv.close(`${day.lunch}`)
-  }
+})
+
+//app.catch or app.fallback
+app.catch((conv, e) => {
+  console.error(e)
+  conv.close(responses.unhandledError)
 })
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app)
-
