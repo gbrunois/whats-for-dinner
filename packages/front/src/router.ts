b8cb606927f1464375f10b40d8495f97f1355652
Geoffrey Brunois <geoffrey.brunois@gmail.com>
2019-08-22 13:32:40 +0200

    integrate privacy policy page in the application (#37)

--- packages/front/src/router.ts
+++ packages/front/src/router.ts
@@ -2,9 +2,11 @@ import store from '@/store'
 import Vue from 'vue'
 import Router, { Route } from 'vue-router'
 import MyPlanningsPage from './views/MyPlannings.vue'
+import PrivacyPolicyPage from './views/PrivacyPolicy.vue'
 import SettingsPage from './views/Settings.vue'
 import SharingsPage from './views/Sharings.vue'
 import SignInPage from './views/SignIn.vue'
+import TermsOfServicePage from './views/TermsOfService.vue'
 import WeekPage from './views/WeekPage.vue'
 
 Vue.use(Router)
@@ -28,6 +30,7 @@ const router = new Router({
       },
     },
     {
+      name: 'sign-in',
       path: '/signIn',
       component: SignInPage,
     },
@@ -52,6 +55,7 @@ const router = new Router({
       name: 'sharings',
       component: SharingsPage,
       meta: {
+        title: 'Mes partages',
         showBackButton: true,
         authRequired: true,
       },
@@ -61,6 +65,7 @@ const router = new Router({
       name: 'settings',
       component: SettingsPage,
       meta: {
+        title: 'Paramètres',
         showBackButton: true,
         authRequired: true,
       },
@@ -70,8 +75,27 @@ const router = new Router({
       name: 'my-plannings',
       component: MyPlanningsPage,
       meta: {
+        title: 'Mes plannings',
         showBackButton: true,
         authRequired: true,
+      },
+    },
+    {
+      path: '/terms-of-service',
+      name: 'terms-of-service',
+      component: TermsOfServicePage,
+      meta: {
+        title: "Conditions d'utilisation",
+        showBackButton: true,
+      },
+    },
+    {
+      path: '/privacy-policy',
+      name: 'privacy-policy',
+      component: PrivacyPolicyPage,
+      meta: {
+        title: 'Politique de confidentialité',
+        showBackButton: true,
       },
     },
   ],
